ff1fb0103e14815163a4a8f3b6bb575e369913e8
Jan Tadeusz Żukowski <4886712@teb-gdansk.ad>
2025-04-16 09:42:36 +0200

    update: added death screen

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManger : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI playerHealthText;
+    public GameObject deathPanel;
     int points;
     int playerHealth;
 
     void Start()
     {
         points = 0;
-        playerHealth = 100;
+        playerHealth = 10;
     }
 
     void Update()
@@ -30,5 +33,17 @@ public class LevelManger : MonoBehaviour
     public void ReducePlayerHeath(int n)
     {
         playerHealth -= n;
+        
+        if (playerHealth <= 0)
+        {
+            Time.timeScale = 0;
+            deathPanel.SetActive(true);
+        }
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
