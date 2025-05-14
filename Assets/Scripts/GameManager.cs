4ceae636edcef2780f75eccadeed5d3445406d5e
Jan Tadeusz Żukowski <4886712@teb-gdansk.ad>
2025-05-14 09:55:01 +0200

    update: added main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+        
+    public void NewGame()
+    {
+        SceneManager.LoadScene("CharacterSelection");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
